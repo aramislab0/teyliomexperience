c7a18cead6c6015287e7ca0b10146279df46709c
Assane Niang <assanearamis@192.168.1.42>
2026-02-09 23:36:04 +0000

    Fix: Added placehold.co and image domains to Next.js config

--- next.config.js
+++ next.config.js
@@ -5,14 +5,28 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'placehold.co',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: '*.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
             },
         ],
-        formats: ['image/avif', 'image/webp'],
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    },
-    experimental: {
-        optimizePackageImports: ['lucide-react', 'framer-motion'],
     },
 }
 
